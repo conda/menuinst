fa81753721014559e987be7ce41b3ea52894a9e1
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-10-29 15:27:14 +0000

    FIX: fixed typo

--- appinst/platforms/win32.py
+++ appinst/platforms/win32.py
@@ -30,7 +30,7 @@ class win32(object):
     # Internal API methods
     #==========================================================================
 
-    def install_application_menus(menus, shortcuts):
+    def _install_application_menus(menus, shortcuts):
         # This determines where the shortcuts will go by checking it the install
         # was for 'All Users' or not
         dir_path = common.get_programs_start_menu()
