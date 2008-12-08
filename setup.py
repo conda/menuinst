9af5ef62e92822a546943a7ef2e5ef439f137712
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-12-08 22:55:58 +0000

    * Fixed bug where AppInst didn't properly require pywin32 on Windows platforms.

--- setup.py
+++ setup.py
@@ -9,16 +9,19 @@ from setuptools import setup, Extension, find_packages
 
 if sys.platform == "win32":
     win_libs = ["comctl32",  "kernel32", "user32", "gdi32", "winspool",
-                "comdlg32", "advapi32", "shell32", "ole32", "oleaut32",
-                "uuid", "odbc32", "odbccp32"]
-
-    extensions = [Extension( "appinst.platforms.wininst",
-                             sources = ["appinst/platforms/wininst.c"],
-                             include_dirs = ["appinst/platforms"],
-                             libraries = win_libs,
-                             )]
+        "comdlg32", "advapi32", "shell32", "ole32", "oleaut32", "uuid",
+        "odbc32", "odbccp32"]
+    extensions = [Extension("appinst.platforms.wininst",
+        sources = ["appinst/platforms/wininst.c"],
+        include_dirs = ["appinst/platforms"],
+        libraries = win_libs,
+        )]
+    install_requires = [
+        'pywin32',
+        ]
 else:
     extensions = []
+    install_requires = []
 
 
 setup(
@@ -28,7 +31,7 @@ setup(
     extras_require = {},
     ext_modules = extensions,
     include_package_data = True,
-    install_requires = [],
+    install_requires = install_requires,
     license = "BSD",
     maintainer = "Enthought, Inc.",
     maintainer_email = "info@enthought.com",
