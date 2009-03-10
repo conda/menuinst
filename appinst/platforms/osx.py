463e566566a70cf8eda014475e0e634927ebf90b
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-10 17:30:06 +0000

    improved osx module to install .app directory tree added application

--- appinst/platforms/osx.py
+++ appinst/platforms/osx.py
@@ -1,14 +1,11 @@
 # Copyright (c) 2008-2009 by Enthought, Inc.
 # All rights reserved.
 
-# TODO: this class should use the osx_application module to create a
-#       an OSX application tree structure, currently only a bash
-#       script (which links to the real application is ceated).
-
 import os
 import sys
 
 from appinst.platforms.shortcut_creation_error import ShortcutCreationError
+from appinst.platforms.osx_application import Application
 
 
 class OSX(object):
@@ -41,7 +38,7 @@ class OSX(object):
         # directories on OS X.  Note that we need to build a map from the menu's
         # category to its path on the filesystem so that we can put the
         # shortcuts in the right directories later.
-        category_map = {}
+        self.category_map = {}
         app_path = '/Applications'
         queue = [(menu_spec, app_path, '') for menu_spec in menus]
         while len(queue) > 0:
@@ -60,7 +57,7 @@ class OSX(object):
                 menu_spec.get('id').capitalize())
             if len(parent_category) > 1:
                 category = '%s.%s' % (parent_category, category)
-            category_map[category] = path
+            self.category_map[category] = path
 
             # Add all sub-menus to the queue so they get created as well.
             for child_spec in menu_spec.get('sub-menus', []):
@@ -73,46 +70,58 @@ class OSX(object):
             # Ensure the shortcut ends up in each of the requested categories.
             for mapped_category in shortcut['categories']:
 
-                # Separate the arguments to the invoked command from the command
-                # itself.   Note that since Finder is automatically launched
-                # when a folder link is selected, and that the default web
-                # browser is launched when a html-like file link is selected,
-                # we can simply strip-out and ignore the special {{FILEBROWSER}}
-                # and {{WEBBROWSER}} placeholders.
-                #
-                # FIXME: Should we instead use Python standard lib's default
-                # webbrowser.py script to open a browser?  We then get to
-                # control whether it opens in a new tab or not.  See the win32
-                # platform support for an example.
-                args = []
-                cmd = shortcut['cmd']
-                if cmd[0] == '{{FILEBROWSER}}' or cmd[0] == '{{WEBBROWSER}}':
-                    del cmd[0]
-                if len(cmd) > 1:
-                    args = cmd[1:]
-                cmd = cmd[0]
+                # Install the actual item
+                self._install_shortcut(mapped_category, shortcut)
 
-                # If the command is a path to an executable file, create a
-                # double-clickable shell script that will execute it.
-                # FIXME: Need to handle assignment of an icon.
-                if os.path.isfile(cmd) and os.access(cmd, os.X_OK):
-                    name = shortcut['name'] + '.command'
-                    path = os.path.join(category_map[mapped_category], name)
-                    f_script = open(path, 'w')
-                    f_script.write(SHELL_SCRIPT % (cmd, ' '.join(args)))
-                    f_script.close()
-                    os.chmod(path, 0755)
+        return
 
-                # Otherwise, just create a symlink to the specified command
-                # value.  Note that it is possible we may only need this logic
-                # as symlinks to executable scripts are double-clickable on
-                # OS X 10.5 (though there would be no way to apply custom icons
-                # then.)
-                else:
-                    name = shortcut['name']
-                    path = os.path.join(category_map[mapped_category], name)
-                    if os.path.exists(path) and os.path.isfile(path):
-                        os.remove(path)
-                    os.symlink(cmd, path)
+
+    def _install_shortcut(self, mapped_category, shortcut):
+
+        # Separate the arguments to the invoked command from the command
+        # itself.   Note that since Finder is automatically launched
+        # when a folder link is selected, and that the default web
+        # browser is launched when a html-like file link is selected,
+        # we can simply strip-out and ignore the special {{FILEBROWSER}}
+        # and {{WEBBROWSER}} placeholders.
+        #
+        # FIXME: Should we instead use Python standard lib's default
+        # webbrowser.py script to open a browser?  We then get to
+        # control whether it opens in a new tab or not.  See the win32
+        # platform support for an example.
+        args = []
+        cmd = shortcut['cmd']
+        if cmd[0] in ('{{FILEBROWSER}}', '{{WEBBROWSER}}'):
+            del cmd[0]
+        if len(cmd) > 1:
+            args = cmd[1:]
+        cmd = cmd[0]
+
+        # If the command is a path to an executable file, create a
+        # double-clickable shell script that will execute it.
+        if os.path.isfile(cmd) and os.access(cmd, os.X_OK):
+
+            name = shortcut['name']
+            path = os.path.join(self.category_map[mapped_category], name)
+
+            if 'icns' in shortcut:
+                incs = shortcut['icns']
+            else:
+                incs = None
+
+            app = Application(name, path, incs)
+            app.create()
+
+        # Otherwise, just create a symlink to the specified command
+        # value.  Note that it is possible we may only need this logic
+        # as symlinks to executable scripts are double-clickable on
+        # OS X 10.5 (though there would be no way to apply custom icons
+        # then.)
+        else:
+            name = shortcut['name']
+            path = os.path.join(self.category_map[mapped_category], name)
+            if os.path.exists(path) and os.path.isfile(path):
+                os.remove(path)
+            os.symlink(cmd, path)
 
         return
