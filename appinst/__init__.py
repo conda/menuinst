f4ace9fcf85a0076bc6f7abaecdb35f7b442c068
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-10 01:08:24 +0000

    added example of an appinst data file which contains a rather detailed description of the data structures it proves.

--- appinst/__init__.py
+++ appinst/__init__.py
@@ -1,1 +1,3 @@
 import application_menus
+
+from api import install_from_dat, uninstall_from_dat
---
+++ appinst/api.py
@@ -0,0 +1,19 @@
+
+
+
+def install_from_dat(path):
+    """
+    given the path to an appinst data file, for an example see
+    examples/appinst.dat, does an complete install.
+    """
+    # TODO
+    pass
+
+
+
+def uninstall_from_dat(path):
+    """
+    given the path to an appinst data file, uninstalls all items.
+    """
+    # TODO
+    pass
