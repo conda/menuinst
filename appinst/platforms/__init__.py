22b358c85fe2097b9c138f19ad497df6ab17c55f
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-20 23:47:55 +0000

    added importing pywin32 DLLs from scripts directory

---
+++ appinst/platforms/__init__.py
@@ -0,0 +1,23 @@
+import sys
+from os.path import isfile, join
+
+
+
+def load_system_module(mod):
+    """
+    Loads the given module from the given directory, inserting the major and
+    minor python version numbers in the module name.
+    """
+    import imp
+
+    fn = "%s%d%d.dll" % (mod, sys.version_info[0], sys.version_info[1])
+    path = join(sys.prefix, 'Scripts', fn)
+    if isilfe(path):
+        imp.load_module(mod, None, path, ('.dll', 'rb', imp.C_EXTENSION))
+    else:
+        print "Warning: %r does not exist" % path
+
+
+if sys.platform == 'win32':
+    load_system_module("pywintypes")
+    load_system_module("pythoncom")
