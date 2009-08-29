300e3c6eb24ecdcb520d9b655364fdac96a9229b
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-08-29 03:58:20 +0000

    trunk is now 2.0.4

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     namespace_packages = [],
     packages = find_packages(),
     package_data = {"appinst" : ["*.txt"], },
-    version = "2.0.3",
+    version = "2.0.4",
     url = "http://www.python.org/sigs/distutils-sig",
     zip_safe = False,
     )
