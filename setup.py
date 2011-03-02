b9615366e2fddd8b4a7b0526c9140823f9fdf809
Ilan Schnell <ischnell@enthought.com>
2011-03-01 22:06:22 -0600

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,37 @@
-# Copyright (c) 2008-2009 by Enthought, Inc.
+# Copyright (c) 2008-2011 by Enthought, Inc.
 # All rights reserved.
-
 
 import sys
 
-from setuptools import Extension, find_packages, setup
+from setuptools import Extension, setup
 
 
 if sys.platform == "win32":
-    win_libs = ["comctl32",  "kernel32", "user32", "gdi32", "winspool",
-        "comdlg32", "advapi32", "shell32", "ole32", "oleaut32", "uuid",
-        "odbc32", "odbccp32"]
-    extensions = [Extension("appinst.platforms.wininst",
-        sources = ["appinst/platforms/wininst.c"],
-        include_dirs = ["appinst/platforms"],
-        libraries = win_libs,
-        )]
-    install_requires = [
-        'pywin32',
-        ]
+    extensions = [Extension(
+            "appinst.wininst",
+            sources=["appinst/wininst.c"],
+            include_dirs=["appinst"],
+            libraries=["comctl32",  "kernel32", "user32", "gdi32", "winspool",
+                       "comdlg32", "advapi32", "shell32", "ole32", "oleaut32",
+                       "uuid", "odbc32", "odbccp32"]
+            )]
+    install_requires = ['pywin32']
 else:
     extensions = []
     install_requires = []
 
 
 setup(
-    author = "Greg Ward, Thomas Heller",
-    author_email = "gward@python.net",
+    name = "AppInst",
+    version = "2.0.5",
     description = "Cross platform APIs used to install applications",
-    extras_require = {},
     ext_modules = extensions,
     include_package_data = True,
+    package_data = {"appinst" : ["*.icns"],},
     install_requires = install_requires,
     license = "BSD",
     maintainer = "Enthought, Inc.",
     maintainer_email = "info@enthought.com",
-    name = "AppInst",
-    namespace_packages = [],
-    packages = find_packages(),
-    package_data = {"appinst" : ["*.txt", "platforms/*.icns"], },
-    version = "2.0.5",
-    url = "http://www.python.org/sigs/distutils-sig",
+    packages = ['appinst'],
     zip_safe = False,
 )
