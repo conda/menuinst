2206ae4f8d57c0f49d72156b21a2a1bae99dee09
Ilan Schnell <ischnell@enthought.com>
2011-03-02 03:31:44 -0600

    update version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
 
 setup(
     name = "AppInst",
-    version = "2.0.5",
+    version = "2.1.0",
     description = "Cross platform APIs used to install applications",
     ext_modules = extensions,
     include_package_data = True,
