a9d68129e2e408079d415b162adb98ede4226d10
Ray Donnelly <mingw.android@gmail.com>
2017-01-23 21:46:08 +0000

    Remove experimental change, update CHANGES.txt

--- cwp.py
+++ cwp.py
@@ -1,7 +1,7 @@
 # this script is used on windows to wrap shortcuts so that they are executed within an environment
 #   It only sets the appropriate prefix PATH entries - it does not actually activate environments
 
-from os
+import os
 import sys
 import subprocess
 from os.path import join, pathsep
@@ -18,20 +18,6 @@ new_paths = pathsep.join([prefix,
                          join(prefix, "Library", "usr", "bin"),
                          join(prefix, "Library", "bin"),
                          join(prefix, "Scripts")])
-
-# Option 2 + 3 (in the try: block) requires a change to conda:
-# https://github.com/conda/conda/pull/4406
-# so option 3 is for older condas, but
-# hardcoding is going to be faster.
-# try:
-#     from conda.cli.activate import get_activate_path
-#     new_paths = get_activate_path(prefix, 'cmd.exe')
-# except:
-#     # Option 3 (I do not like hacking creationflags here)
-#     CREATE_NO_WINDOW=0x08000000
-#     new_paths = str(subprocess.check_output([join(prefix, "Scripts", "conda"), '..activate', 'cmd.exe', prefix],
-#                                             creationflags=CREATE_NO_WINDOW))
-
 env = os.environ.copy()
 env['PATH'] = new_paths + pathsep + env['PATH']
 env['CONDA_PREFIX'] = prefix
