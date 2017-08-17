751d3820ed30809cc82c0988f08730e1301a8e6b
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-08-17 14:08:02 -0400

    Update tests

--- tests/test_menu_creation.py
+++ tests/test_menu_creation.py
@@ -1,15 +1,28 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2013-2017 Continuum Analytics, Inc.
+# Copyright (c) 2008-2011 by Enthought, Inc.
+# All rights reserved.
+#
+# Licensed under the terms of the BSD 3-clause License (See LICENSE.txt)
+# -----------------------------------------------------------------------------
+"""Menuinst tests."""
+
+# Standard library imports
 import os
 import sys
 
+# Third party imports
 import pytest
 
+# Local imports
 import menuinst
 
 
 menu_dir = os.path.dirname(__file__)
 
-@pytest.mark.skipif(sys.platform != "win32",
-                    reason="Windows-only tests")
+
+@pytest.mark.skipif(sys.platform != "win32", reason="Windows-only tests")
 class TestWindowsShortcuts(object):
 
     def test_install_folders_exist(self):
@@ -25,8 +38,8 @@ class TestWindowsShortcuts(object):
         for mode in ["user", "system"]:
             if mode=="user":
                 open(nonadmin, 'a').close()
-            menuinst.install(shortcut, remove=False)
-            menuinst.install(shortcut, remove=True)
+            menuinst.api.install(shortcut, remove=False)
+            menuinst.api.install(shortcut, remove=True)
             if os.path.exists(nonadmin):
                 os.remove(nonadmin)
         if has_nonadmin:
