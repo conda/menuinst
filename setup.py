57cd0c00fa2dae47f5fc47ef25248f619b5eb7e2
Jannis Leidel <jannis@leidel.info>
2021-07-07 18:34:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ else:
 
 setup(
     name = "menuinst",
-    url = "https://github.com/ContinuumIO/menuinst",
+    url = "https://github.com/conda/menuinst",
     version = versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description = "cross platform install of menu items",
