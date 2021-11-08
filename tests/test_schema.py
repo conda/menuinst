c4c79ec1a46781a06bf7b963e30eb6991631ec2d
Jaime RGP <jaimergp@users.noreply.github.com>
2021-11-08 22:09:10 +0100

    supress slow warnings

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -8,8 +8,8 @@ from pydantic import ValidationError
 from menuinst.schema import MenuInstSchema, validate
 from conftest import DATA
 
-
-@settings(max_examples=100)
+# suppress_health_check=3 --> too_slow
+@settings(max_examples=100, suppress_health_check=3)
 @given(from_schema(MenuInstSchema.schema()))
 def test_schema_can_be_loaded(value):
     assert value
