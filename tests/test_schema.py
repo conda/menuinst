16ce9133ee05e5d2fe64bed0daf35ce421d49197
Jaime RGP <jaimergp@users.noreply.github.com>
2021-11-08 22:34:07 +0100

    better arguments

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 import pytest
-from hypothesis import given, settings
+from hypothesis import given, settings, HealthCheck
 from hypothesis_jsonschema import from_schema
 from pydantic import ValidationError
 
@@ -9,7 +9,7 @@ from menuinst.schema import MenuInstSchema, validate
 from conftest import DATA
 
 # suppress_health_check=3 --> too_slow
-@settings(max_examples=100, suppress_health_check=[3])
+@settings(max_examples=100, suppress_health_check=[HealthCheck.too_slow])
 @given(from_schema(MenuInstSchema.schema()))
 def test_schema_can_be_loaded(value):
     assert value
