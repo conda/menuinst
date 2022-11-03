323406187b02190b28e5843655e28160c68ce43d
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-11-03 17:01:04 +0100

    more robust slugs

--- menuinst/utils.py
+++ menuinst/utils.py
@@ -1,37 +1,43 @@
 import os
+import re
 import shlex
 import xml.etree.ElementTree as XMLTree
 from pathlib import Path
+from unicodedata import normalize
 
 
 def slugify(text):
-    non_url_safe = (
-        '"',
-        "#",
-        "$",
-        "%",
-        "&",
-        "+",
-        ",",
-        "/",
-        ":",
-        ";",
-        "=",
-        "?",
-        "@",
-        "[",
-        "\\",
-        "]",
-        "^",
-        "`",
-        "{",
-        "|",
-        "}",
-        "~",
-        "'",
-    )
-    translate_table = {ord(char): "" for char in non_url_safe}
-    return "_".join(text.translate(translate_table).split())
+    # Adapted from from django.utils.text.slugify
+    # Copyright (c) Django Software Foundation and individual contributors.
+    # All rights reserved.
+    # Redistribution and use in source and binary forms, with or without modification,
+    # are permitted provided that the following conditions are met:
+    #     1. Redistributions of source code must retain the above copyright notice,
+    #     this list of conditions and the following disclaimer.
+    #     2. Redistributions in binary form must reproduce the above copyright
+    #     notice, this list of conditions and the following disclaimer in the
+    #     documentation and/or other materials provided with the distribution.
+    #     3. Neither the name of Django nor the names of its contributors may be used
+    #     to endorse or promote products derived from this software without
+    #     specific prior written permission.
+    # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    # DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+    # ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+    # ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    """
+    Convert to ASCII, and spaces to hyphens.
+    Remove characters that aren't alphanumerics, underscores, or hyphens.
+    Convert to lowercase. Also strip leading and trailing whitespace.
+    """
+    text = normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii')
+    text = re.sub('[^\w\s-]', '', text).strip().lower()
+    return re.sub('[-\s]+', '-', text)
 
 
 def indent_xml_tree(elem, level=0):
