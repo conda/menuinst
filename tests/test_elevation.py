f205dedf687ae2923722a9ff7e6bba0dc5ac9554
jaimergp <jaimergp@users.noreply.github.com>
2023-07-04 11:23:16 +0200

    read from file

--- tests/test_elevation.py
+++ tests/test_elevation.py
@@ -4,13 +4,26 @@ from menuinst.utils import _test_elevation, elevate_as_needed, user_is_admin
 
 
 def test_elevation(tmp_path, capfd):
-    if os.name == "nt" and os.environ.get("CI"):
-        # Windows runners on GHA always run as admin
+    if os.name == "nt":
+        on_ci = os.environ.get("CI")
+        is_admin = user_is_admin()
+        if not on_ci:
+            # Windows runners on GHA always run as admin
+            assert not is_admin
+        
         _test_elevation(str(tmp_path))
-        assert capfd.readouterr().out.strip().endswith("_mode: user")
+        output = (tmp_path / "_test_output.txt").read_text().strip()
+        if on_ci:
+            assert output.endswith("_mode: user")
+        else:
+            assert output.endswith("user_is_admin(): False _mode: user")
 
         elevate_as_needed(_test_elevation)(base_prefix=str(tmp_path))
-        assert capfd.readouterr().out.strip().endswith("_mode: system")
+        output = (tmp_path / "_test_output.txt").read_text().strip()
+        if on_ci:
+            assert output.endswith("_mode: system")
+        else:
+            assert output.endswith("user_is_admin(): True _mode: system")
     else:
         assert not user_is_admin()  # We need to start as a non-root user
 
