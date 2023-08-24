4ef43fdfa5c46d81d39481a1ad20dfb9f8decae7
jaimergp <jaimergp@users.noreply.github.com>
2023-08-24 17:59:38 +0200

    reorganize a bit to facilitate exports

--- menuinst/__init__.py
+++ menuinst/__init__.py
@@ -1,73 +1,28 @@
 """
 """
 
-import json
 import os
-import sys
 from logging import basicConfig as _basicConfig
-from logging import getLogger as _getLogger
-from os import PathLike
+
+import apipkg as _apipkg
 
 try:
     from ._version import __version__
 except ImportError:
     __version__ = "dev"
 
-
-from .utils import DEFAULT_BASE_PREFIX, DEFAULT_PREFIX
-
-_log = _getLogger(__name__)
-
+from .api import _install_adapter as install  # noqa
 
 if os.environ.get("MENUINST_DEBUG"):
     _basicConfig(level="DEBUG")
 
-
-def install(path: PathLike, remove: bool = False, prefix: PathLike = DEFAULT_PREFIX, **kwargs):
-    """
-    This function is only here as a legacy adapter for menuinst v1.x.
-    Please use `menuinst.api` functions instead.
-    """
-    if sys.platform == "win32":
-        path = path.replace("/", "\\")
-    json_path = os.path.join(prefix, path)
-    with open(json_path) as f:
-        metadata = json.load(f)
-    if "$id" not in metadata:  # old style JSON
-        from ._legacy import install as _legacy_install
-
-        if sys.platform == "win32":
-            kwargs.setdefault("root_prefix", kwargs.pop("base_prefix", DEFAULT_BASE_PREFIX))
-            if kwargs["root_prefix"] is None:
-                kwargs["root_prefix"] = DEFAULT_BASE_PREFIX
-            _legacy_install(json_path, remove=remove, prefix=prefix, **kwargs)
-        else:
-            _log.warn(
-                "menuinst._legacy is only supported on Windows. "
-                "Switch to the new-style menu definitions "
-                "for cross-platform compatibility."
-            )
-    else:
-        from .api import install as _api_install
-        from .api import remove as _api_remove
-
-        # patch kwargs to reroute root_prefix to base_prefix
-        kwargs.setdefault("base_prefix", kwargs.pop("root_prefix", DEFAULT_BASE_PREFIX))
-        if kwargs["base_prefix"] is None:
-            kwargs["base_prefix"] = DEFAULT_BASE_PREFIX
-        if remove:
-            _api_remove(metadata, target_prefix=prefix, **kwargs)
-        else:
-            _api_install(metadata, target_prefix=prefix, **kwargs)
-
-
+# Compatibility forwarders for menuinst v1.x (Windows only)
 if os.name == "nt":
-    # Compatibility forwarders for menuinst v1.x (Windows only)
-    import apipkg as _apipkg
-
+    # Calling initpkg clears the 'menuinst' top-level namespace and replaces it with exportdefs
+    # if we want to keep something defined here, use the attr dictionary
     _apipkg.initpkg(
         __name__,
-        {
+        exportdefs={
             "win32": {
                 "dirs_src": "menuinst.platforms.win_utils.knownfolders:dirs_src",
             },
@@ -83,4 +38,5 @@ if os.name == "nt":
                 "isUserAdmin": "menuinst.platforms.win_utils.win_elevate:isUserAdmin",
             },
         },
+        attr={"__version__": __version__, "install": install},
     )
--- tests/test_backwards_compatibility.py
+++ tests/test_backwards_compatibility.py
@@ -6,6 +6,7 @@ import pytest
 @pytest.mark.skipif(os.name != "nt", reason="Windows only")
 def test_import_paths():
     "These import paths are/were used by conda <=23.7.2. Ensure they still work."
+    from menuinst import install # noqa
     from menuinst.knownfolders import FOLDERID, get_folder_path  # noqa
     from menuinst.win32 import dirs_src  # noqa
     from menuinst.winshortcut import create_shortcut  # noqa
