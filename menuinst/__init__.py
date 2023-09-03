78c4041e8f59bcf1cd0dd3ad137ec33f4e2182de
jaimergp <jaimergp@users.noreply.github.com>
2023-09-03 12:09:31 +0200

    Use `apipkg` to provide compatibility forwarders with v1 (#151)
    * Use apipkg to provide compatibility forwarders with v1
    
    * add simple test
    
    * add news
    
    * patch conda via pip; add apipkg
    
    * pre-commit
    
    * try a different approach
    
    * remove unused key
    
    * yet one more
    
    * quote < symbols
    
    * quotess
    
    * reorganize a bit to facilitate exports
    
    * pre-commit
    
    * move comment
    
    * reorganize again
    
    * move again to api
    
    * add missing import
    
    * vendor apipkg
    
    * pre-commit
    
    * Update apipkg to v3.0.1
    
    * fix import
    
    * vendor via vendoring pkg

--- menuinst/__init__.py
+++ menuinst/__init__.py
@@ -1,59 +1,47 @@
 """
 """
 
-import json
 import os
-import sys
-from logging import basicConfig as _basicConfig
-from logging import getLogger as _getLogger
-from os import PathLike
+from logging import basicConfig, getLogger
+
+from .api import _install_adapter as install
 
 try:
     from ._version import __version__
 except ImportError:
     __version__ = "dev"
 
-
-from ._legacy import install as _legacy_install
-from .api import install as _api_install
-from .api import remove as _api_remove
-from .utils import DEFAULT_BASE_PREFIX, DEFAULT_PREFIX
-
-_log = _getLogger(__name__)
-
-
+log = getLogger(__name__)
 if os.environ.get("MENUINST_DEBUG"):
-    _basicConfig(level="DEBUG")
+    basicConfig(level="DEBUG")
+
+__all__ = ["install", "__version__"]
 
 
-def install(path: PathLike, remove: bool = False, prefix: PathLike = DEFAULT_PREFIX, **kwargs):
-    """
-    This function is only here as a legacy adapter for menuinst v1.x.
-    Please use `menuinst.api` functions instead.
-    """
-    if sys.platform == "win32":
-        path = path.replace("/", "\\")
-    json_path = os.path.join(prefix, path)
-    with open(json_path) as f:
-        metadata = json.load(f)
-    if "$id" not in metadata:  # old style JSON
-        if sys.platform == "win32":
-            kwargs.setdefault("root_prefix", kwargs.pop("base_prefix", DEFAULT_BASE_PREFIX))
-            if kwargs["root_prefix"] is None:
-                kwargs["root_prefix"] = DEFAULT_BASE_PREFIX
-            _legacy_install(json_path, remove=remove, prefix=prefix, **kwargs)
-        else:
-            _log.warn(
-                "menuinst._legacy is only supported on Windows. "
-                "Switch to the new-style menu definitions "
-                "for cross-platform compatibility."
-            )
-    else:
-        # patch kwargs to reroute root_prefix to base_prefix
-        kwargs.setdefault("base_prefix", kwargs.pop("root_prefix", DEFAULT_BASE_PREFIX))
-        if kwargs["base_prefix"] is None:
-            kwargs["base_prefix"] = DEFAULT_BASE_PREFIX
-        if remove:
-            _api_remove(metadata, target_prefix=prefix, **kwargs)
-        else:
-            _api_install(metadata, target_prefix=prefix, **kwargs)
+# Compatibility forwarders for menuinst v1.x (Windows only)
+if os.name == "nt":
+    from ._vendor.apipkg import initpkg
+
+    initpkg(
+        __name__,
+        exportdefs={
+            "win32": {
+                "dirs_src": "menuinst.platforms.win_utils.knownfolders:dirs_src",
+            },
+            "knownfolders": {
+                "get_folder_path": "menuinst.platforms.win_utils.knownfolders:get_folder_path",
+                "FOLDERID": "menuinst.platforms.win_utils.knownfolders:FOLDERID",
+            },
+            "winshortcut": {
+                "create_shortcut": "menuinst.platforms.win_utils.winshortcut:create_shortcut",
+            },
+            "win_elevate": {
+                "runAsAdmin": "menuinst.platforms.win_utils.win_elevate:runAsAdmin",
+                "isUserAdmin": "menuinst.platforms.win_utils.win_elevate:isUserAdmin",
+            },
+        },
+        # Calling initpkg WILL CLEAR the 'menuinst' top-level namespace, and only then will add
+        # the exportdefs contents! If we want to keep something defined in this module, we MUST
+        # make sure it's added in the 'attr' dictionary below.
+        attr={"__version__": __version__, "install": install},
+    )
--- menuinst/api.py
+++ menuinst/api.py
@@ -2,10 +2,10 @@
 """
 
 import json
+import os
 import sys
 import warnings
 from logging import getLogger
-from os import PathLike
 from pathlib import Path
 from typing import Any, Callable, List, Optional, Tuple, Union
 
@@ -24,9 +24,9 @@ __all__ = [
 
 
 def _load(
-    metadata_or_path: Union[PathLike, dict],
-    target_prefix: Optional[PathLike] = None,
-    base_prefix: Optional[PathLike] = None,
+    metadata_or_path: Union[os.PathLike, dict],
+    target_prefix: Optional[os.PathLike] = None,
+    base_prefix: Optional[os.PathLike] = None,
     _mode: _UserOrSystem = "user",
 ) -> Tuple[Menu, List[MenuItem]]:
     target_prefix = target_prefix or DEFAULT_PREFIX
@@ -43,12 +43,12 @@ def _load(
 
 @elevate_as_needed
 def install(
-    metadata_or_path: Union[PathLike, dict],
+    metadata_or_path: Union[os.PathLike, dict],
     *,
-    target_prefix: Optional[PathLike] = None,
-    base_prefix: Optional[PathLike] = None,
+    target_prefix: Optional[os.PathLike] = None,
+    base_prefix: Optional[os.PathLike] = None,
     _mode: _UserOrSystem = "user",
-) -> List[PathLike]:
+) -> List[os.PathLike]:
     target_prefix = target_prefix or DEFAULT_PREFIX
     base_prefix = base_prefix or DEFAULT_BASE_PREFIX
     menu, menu_items = _load(metadata_or_path, target_prefix, base_prefix, _mode)
@@ -66,12 +66,12 @@ def install(
 
 @elevate_as_needed
 def remove(
-    metadata_or_path: Union[PathLike, dict],
+    metadata_or_path: Union[os.PathLike, dict],
     *,
-    target_prefix: Optional[PathLike] = None,
-    base_prefix: Optional[PathLike] = None,
+    target_prefix: Optional[os.PathLike] = None,
+    base_prefix: Optional[os.PathLike] = None,
     _mode: _UserOrSystem = "user",
-) -> List[PathLike]:
+) -> List[os.PathLike]:
     target_prefix = target_prefix or DEFAULT_PREFIX
     base_prefix = base_prefix or DEFAULT_BASE_PREFIX
     menu, menu_items = _load(metadata_or_path, target_prefix, base_prefix, _mode)
@@ -90,11 +90,11 @@ def remove(
 @elevate_as_needed
 def install_all(
     *,
-    target_prefix: Optional[PathLike] = None,
-    base_prefix: Optional[PathLike] = None,
+    target_prefix: Optional[os.PathLike] = None,
+    base_prefix: Optional[os.PathLike] = None,
     filter: Union[Callable, None] = None,
     _mode: _UserOrSystem = "user",
-) -> List[List[PathLike]]:
+) -> List[List[os.PathLike]]:
     target_prefix = target_prefix or DEFAULT_PREFIX
     base_prefix = base_prefix or DEFAULT_BASE_PREFIX
     return _process_all(install, target_prefix, base_prefix, filter, _mode)
@@ -103,11 +103,11 @@ def install_all(
 @elevate_as_needed
 def remove_all(
     *,
-    target_prefix: Optional[PathLike] = None,
-    base_prefix: Optional[PathLike] = None,
+    target_prefix: Optional[os.PathLike] = None,
+    base_prefix: Optional[os.PathLike] = None,
     filter: Union[Callable, None] = None,
     _mode: _UserOrSystem = "user",
-) -> List[List[Union[str, PathLike]]]:
+) -> List[List[Union[str, os.PathLike]]]:
     target_prefix = target_prefix or DEFAULT_PREFIX
     base_prefix = base_prefix or DEFAULT_BASE_PREFIX
     return _process_all(remove, target_prefix, base_prefix, filter, _mode)
@@ -115,8 +115,8 @@ def remove_all(
 
 def _process_all(
     function: Callable,
-    target_prefix: Optional[PathLike] = None,
-    base_prefix: Optional[PathLike] = None,
+    target_prefix: Optional[os.PathLike] = None,
+    base_prefix: Optional[os.PathLike] = None,
     filter: Union[Callable, None] = None,
     _mode: _UserOrSystem = "user",
 ) -> List[Any]:
@@ -128,3 +128,43 @@ def _process_all(
         if filter is not None and filter(path):
             results.append(function(path, target_prefix, base_prefix, _mode))
     return results
+
+
+_api_remove = remove  # alias to prevent shadowing in the function below
+
+
+def _install_adapter(
+    path: os.PathLike, remove: bool = False, prefix: os.PathLike = DEFAULT_PREFIX, **kwargs
+):
+    """
+    This function is only here as a legacy adapter for menuinst v1.x.
+    Please use `menuinst.api` functions instead.
+    """
+    if os.name == "nt":
+        path = path.replace("/", "\\")
+    json_path = os.path.join(prefix, path)
+    with open(json_path) as f:
+        metadata = json.load(f)
+    if "$id" not in metadata:  # old style JSON
+        from ._legacy import install as _legacy_install
+
+        if os.name == "nt":
+            kwargs.setdefault("root_prefix", kwargs.pop("base_prefix", DEFAULT_BASE_PREFIX))
+            if kwargs["root_prefix"] is None:
+                kwargs["root_prefix"] = DEFAULT_BASE_PREFIX
+            _legacy_install(json_path, remove=remove, prefix=prefix, **kwargs)
+        else:
+            log.warn(
+                "menuinst._legacy is only supported on Windows. "
+                "Switch to the new-style menu definitions "
+                "for cross-platform compatibility."
+            )
+    else:
+        # patch kwargs to reroute root_prefix to base_prefix
+        kwargs.setdefault("base_prefix", kwargs.pop("root_prefix", DEFAULT_BASE_PREFIX))
+        if kwargs["base_prefix"] is None:
+            kwargs["base_prefix"] = DEFAULT_BASE_PREFIX
+        if remove:
+            _api_remove(metadata, target_prefix=prefix, **kwargs)
+        else:
+            install(metadata, target_prefix=prefix, **kwargs)
