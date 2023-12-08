fdb62d9542e1ed4bdd115494133b7e3da7964cf4
Ken Odegard <kodegard@anaconda.com>
2023-12-08 17:20:19 -0600

    More tests

--- tests/test_backwards_compatibility.py
+++ tests/test_backwards_compatibility.py
@@ -5,8 +5,16 @@ import pytest
 
 @pytest.mark.skipif(os.name != "nt", reason="Windows only")
 def test_import_paths():
-    "These import paths are/were used by conda <=23.7.2. Ensure they still work."
-    from menuinst import install  # noqa
-    from menuinst.knownfolders import FOLDERID, get_folder_path  # noqa
-    from menuinst.win32 import dirs_src  # noqa
-    from menuinst.winshortcut import create_shortcut  # noqa
+    """Imports used by conda <=23.7.2. Ensure they still work."""
+    # ensure apipkg generated submodules work
+    from menuinst import install  # noqa: F401
+    from menuinst.knownfolders import FOLDERID, get_folder_path  # noqa: F401
+    from menuinst.win32 import dirs_src  # noqa: F401
+    from menuinst.winshortcut import create_shortcut  # noqa: F401
+    from menuinst.win_elevate import runAsAdmin, isUserAdmin  # noqa: F401
+
+    # ensure legacy submodules work
+    import menuinst._legacy.cwd  # noqa: F401
+    import menuinst._legacy.main  # noqa: F401
+    import menuinst._legacy.utils  # noqa: F401
+    import menuinst._legacy.win32  # noqa: F401
