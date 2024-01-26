448cb83e6bac0b5561344ab8f674a81039aacf72
jaimergp <jaimergp@users.noreply.github.com>
2024-01-26 16:06:53 +0100

    Update tests/_legacy/test_cwp.py

--- tests/_legacy/test_cwp.py
+++ tests/_legacy/test_cwp.py
@@ -1,11 +1,21 @@
 import sys
+from subprocess import check_output
 
 import pytest
 
 cwp = pytest.importorskip("menuinst._legacy.cwp", reason="Windows only")
 
 
-def test_cwp(capsys):
-    with pytest.raises(SystemExit):
-        cwp.main([sys.prefix, "python", "-c", "print('hello')"])
-        assert capsys.readouterr() == ("hello\n", "")
+def test_cwp():
+    out = check_output(
+        [
+            sys.executable,
+            cwp.__file__,
+            sys.prefix,
+            "python",
+            "-c",
+            "import sys; print(sys.prefix)",
+        ],
+        text=True,
+    )
+    assert out.strip() == sys.prefix
