c10b98f38d9912cb1c594049aaf6d4976dba53c9
Robin <randersson@anaconda.com>
2025-10-30 11:09:19 -0400

    Fix format

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,25 +1,20 @@
 """
-    Tests for specific functions in the utils package.
+Tests for specific functions in the utils package.
 """
+
 from menuinst.utils import WinLex
 
 
 def test_quote_args_1():
-    """ Verify the input arguments are quoted. """
+    """Verify the input arguments are quoted."""
     wl = WinLex()
     test_str = [r"%SystemRoot%\system32\foo.exe", "/pt", "%1", "%2", "%3", "%4"]
     output = wl.quote_args(test_str)
-    assert output == [
-        '%SystemRoot%\\system32\\foo.exe',
-        '"/pt"',
-        '"%1"',
-        '"%2"',
-        '"%3"',
-        '"%4"'
-    ]
+    assert output == ["%SystemRoot%\\system32\\foo.exe", '"/pt"', '"%1"', '"%2"', '"%3"', '"%4"']
+
 
 def test_quote_args_2():
-    """ Verify correct quotes with blank spaces. """
+    """Verify correct quotes with blank spaces."""
     wl = WinLex()
     args = [
         r"C:\Windows\System32\notepad.exe",
@@ -37,12 +32,8 @@ def test_quote_args_2():
 
 
 def test_quote_args_3():
-    """ Verify special case with cmd.exe and /C. """
+    """Verify special case with cmd.exe and /C."""
     args = ["cmd.exe", "/C", "echo", "Hello World"]
     wl = WinLex()
     output = wl.quote_args(args)
-    assert output == [
-        '"cmd.exe"',
-        '/C',
-        '""echo" "Hello World""'
-    ]
+    assert output == ['"cmd.exe"', "/C", '""echo" "Hello World""']
